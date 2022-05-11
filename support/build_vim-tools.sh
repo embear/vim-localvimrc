244a92ceae63b8c23a74022eaf205b431745fcb6
Markus Braun <markus.braun@krawel.de>
2022-05-11 19:41:45 +0200

    switch to a maintained fork of vim-tools

--- support/build_vim-tools.sh
+++ support/build_vim-tools.sh
@@ -3,9 +3,9 @@
 
 # package settings
 PACKAGE=vim-tools
-REPO="https://github.com/xolox/vim-tools.git"
+REPO="https://github.com/ycm-core/vim-tools"
 HERE=$(readlink -f "$(dirname $0)")
-DEST=$(readlink -f "$HERE/vim-tools")
+DEST=$(readlink -f "${HERE}/vim-tools")
 
 # die()
 die()
@@ -16,22 +16,18 @@ die()
 
 # get source code
 echo "getting sources ..."
-git clone "$REPO" "$DEST" || die "get sources failed"
-
-# patch usage of outdated coloredlogs API
-cd $DEST
-patch -p1 < $HERE/vim-tools.patch || die "patching files failed"
+git clone "${REPO}" "${DEST}" || die "get sources failed"
 
 # virtualenv
 echo "creating virtualenv ..."
-virtualenv $DEST/virtualenv || die "virtualenv failed"
-$DEST/virtualenv/bin/pip install beautifulsoup coloredlogs markdown || die "virtualenv failed"
+virtualenv ${DEST}/virtualenv || die "virtualenv failed"
+${DEST}/virtualenv/bin/pip install -r ${DEST}/requirements.txt || die "virtualenv failed"
 
 # executable
-mkdir $DEST/bin
-cat > $DEST/bin/html2vimdoc <<EOF
+mkdir ${DEST}/bin
+cat > ${DEST}/bin/html2vimdoc <<EOF
 #!/bin/sh
-. $DEST/virtualenv/bin/activate
-$DEST/html2vimdoc.py \$*
+. ${DEST}/virtualenv/bin/activate
+${DEST}/html2vimdoc.py \$*
 EOF
-chmod 0755 $DEST/bin/html2vimdoc
+chmod 0755 ${DEST}/bin/html2vimdoc
